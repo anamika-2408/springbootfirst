5df0506dbb5d17969845804bd8f1df9f80329c69
anamika-2408 <78497091+anamika-2408@users.noreply.github.com>
2022-12-11 17:35:30 +0530

    Add files via upload

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.exception;
+
+public class ResourceNotFoundException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ UserController.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.controller;
+
+public class UserController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
